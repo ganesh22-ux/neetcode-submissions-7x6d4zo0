3c46afc3d482dacefac55cc0fcd1a805899d6ff8
U Shree Sai Ganesh <usrisaiganesh@gmail.com>
2026-06-29 10:35:46 +0530

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int n=heights.size();
+        int l=0,r=n-1;
+        int ans=0;
+        int maxium;
+        for(int i=0;i<n-1;i++){
+           ans=l*heights[r];
+           maxium=max(maxium,ans);
+        }
+        return max;
+    }
+};
