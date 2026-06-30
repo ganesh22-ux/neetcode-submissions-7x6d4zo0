92fa126a3b9c5a7f0106b473f5cc87cda70eb226
U Shree Sai Ganesh <usrisaiganesh@gmail.com>
2026-06-30 19:38:36 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxp=0;
+        int minb=prices[i];
+        for(int &sell :prices){
+            maxp=max(maxp,sell-minb);
+            minb=min(minb,sell);
+
+        }
+        return maxp;
+    }
+};
