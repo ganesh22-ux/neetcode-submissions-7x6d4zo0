e5a9f10d3f22b98e62b46c6b2a936f38bc5f0300
U Shree Sai Ganesh <usrisaiganesh@gmail.com>
2026-08-26 23:03:42 +0530

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if(strs.empty()) return "";
+        for(int i=0;i<strs[0].size();i++){
+            for( const string & s: strs){
+                if(i==s.length()s[i] !=strs[0][i]){
+                    return s.substr(0,i);
+                }
+            }
+        }
+        return strs[0];
+    }
+};
